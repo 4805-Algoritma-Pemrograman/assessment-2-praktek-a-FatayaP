f245c91d578c2da2b48ab74ff8aac35fa82d8160
FatayaP <110081846+FatayaP@users.noreply.github.com>
2024-12-12 08:41:31 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
-public class Nomor1 {
+public class AngkaGanjil {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan bilangan n: ");
+        int n = scanner.nextInt();
+        scanner.close();
 
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-        tampilkanAngkaGanjil(N);
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+        tampilkanAngkaGanjil(n);
     }
-    public static void tampilkanAngkaGanjil(int N) {
-        for (int i = 0; i <= N; i++) {
+    public static void tampilkanAngkaGanjil(int n) {
+        for (int i = 0; i <= n; i++) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
     }
 }
+
