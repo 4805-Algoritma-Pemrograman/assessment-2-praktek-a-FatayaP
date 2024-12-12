2fc7ee04f69b774fe48fa3d659df37a38688d0ea
FatayaP <110081846+FatayaP@users.noreply.github.com>
2024-12-12 08:43:44 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,13 +1,64 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nomor2 {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
+    static ArrayList<Integer> keranjang = new ArrayList<>();
 
-        /* Pekerjaan anda berakhir sini */
+    public static void main(String[] args) {
+        int[] hargaMenu = {35000, 45000, 20000, 15000};
+        String[] namaMenu = {"Burger", "Steak", "Spaghetti", "Kentang"};
 
-        System.out.println(Nomor2.class.getDeclaredMethods().length); 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Masukan nama menu [Burger/Steak/Spaghetti/Kentang]:");
+            String inputMenu = scanner.nextLine();
+
+            int indexMenu = -1;
+            for (int i = 0; i < namaMenu.length; i++) {
+                if (namaMenu[i].equalsIgnoreCase(inputMenu)) {
+                    indexMenu = i;
+                    break;
+                }
+            }
+
+            if (indexMenu != -1) {
+                keranjang.add(hargaMenu[indexMenu]);
+            } else {
+                System.out.println("Menu tidak ditemukan!");
+            }
+
+            System.out.print("Input lagi (Y/N)?: ");
+            String inputLagi = scanner.nextLine();
+
+            if (inputLagi.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+
+        int totalBelanja = hitungTotalBelanja();
+        int diskon = hitungDiskon(totalBelanja);
+        int totalBayar = totalBelanja - diskon;
+
+        System.out.println("Total item pesanan = " + keranjang.size());
+        System.out.println("Total yang harus dibayar = " + totalBayar);
+    }
+
+    public static int hitungTotalBelanja() {
+        int total = 0;
+        for (int harga : keranjang) {
+            total += harga;
+        }
+        return total;
+    }
+
+    public static int hitungDiskon(int totalBelanja) {
+        if (totalBelanja < 50000) {
+            return 0;
+        } else if (totalBelanja >= 50000 && totalBelanja <= 100000) {
+            return (int) (totalBelanja * 0.05);
+        } else {
+            return (int) (totalBelanja * 0.1);
+        }
     }
 }
